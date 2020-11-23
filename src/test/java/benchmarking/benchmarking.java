d36b114a004a0086029f8340ae569f4ba80cd19c
Lukas Bach <lukas.bach@visma.com>
2020-11-23 13:06:35 +0100

    PR

--- src/test/java/benchmarking/benchmarking.java
+++ src/test/java/benchmarking/benchmarking.java
@@ -124,8 +124,8 @@ public class benchmarking extends JUnitTestAbstract {
                 syncedTasksAllStartTime.put(task, task.getStartTime());
 
         IShift shift = new TestShift(3600 * 10, 0, 3600 * 10);
-        for (int i = 0; i < 1; i++) {
-            for (int j = 0; j < 1; j++) {
+        for (int i = 0; i < 10000; i++) {
+            for (int j = 0; j < 100; j++) {
                 routeEvaluator1.evaluateRouteByOrderOfTasksWithObjectiveValues(newTasks, syncedTasksNewStartTime, shift);
 
                 routeEvaluator2.evaluateRouteByOrderOfTasksWithObjectiveValues(newTasks, syncedTasksNewStartTime, shift);
