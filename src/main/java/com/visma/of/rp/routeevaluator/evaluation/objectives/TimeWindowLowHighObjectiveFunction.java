0a426b300b1d24bf46a23646e5af8be6f0e6ee2b
Lukas Bach <lukas.bach@visma.com>
2021-11-17 15:06:04 +0100

    Default time window objective values changed

--- src/main/java/com/visma/of/rp/routeevaluator/evaluation/objectives/TimeWindowLowHighObjectiveFunction.java
+++ src/main/java/com/visma/of/rp/routeevaluator/evaluation/objectives/TimeWindowLowHighObjectiveFunction.java
@@ -5,6 +5,15 @@ import com.visma.of.rp.routeevaluator.interfaces.IObjectiveFunctionIntraRoute;
 
 public class TimeWindowLowHighObjectiveFunction implements IObjectiveFunctionIntraRoute {
 
+    public static final double DEFAULT_PENALTY_FOR_BREAKING_TIME_WINDOW = .5;
+    public static final double DEFAULT_PENALTY_FOR_BREAKING_TIME_WINDOW_HIGH = 2;
+    public static final long DEFAULT_PENALTY_FOR_HIGH_TIME_WINDOW_CUT_OFF = 600;
+
+    /**
+     * Multiplier applied inside the cut off, hence breaking the time window by more than
+     * the cut off is penalized x times more, than inside the cut off.
+     */
+    private final double lowPenaltyMultiplier;
     /**
      * Cut off at which the high penalty is applied.
      */
@@ -18,6 +27,19 @@ public class TimeWindowLowHighObjectiveFunction implements IObjectiveFunctionInt
     public TimeWindowLowHighObjectiveFunction(long highPenaltyCutOff, double highPenaltyMultiplier) {
         this.highPenaltyCutOff = highPenaltyCutOff;
         this.highPenaltyMultiplier = highPenaltyMultiplier;
+        this.lowPenaltyMultiplier = DEFAULT_PENALTY_FOR_BREAKING_TIME_WINDOW;
+    }
+
+    public TimeWindowLowHighObjectiveFunction(double lowPenaltyMultiplier, long highPenaltyCutOff, double highPenaltyMultiplier) {
+        this.highPenaltyCutOff = highPenaltyCutOff;
+        this.highPenaltyMultiplier = highPenaltyMultiplier;
+        this.lowPenaltyMultiplier = lowPenaltyMultiplier;
+    }
+
+    public TimeWindowLowHighObjectiveFunction() {
+        this.highPenaltyCutOff = DEFAULT_PENALTY_FOR_HIGH_TIME_WINDOW_CUT_OFF;
+        this.highPenaltyMultiplier = DEFAULT_PENALTY_FOR_BREAKING_TIME_WINDOW_HIGH;
+        this.lowPenaltyMultiplier = DEFAULT_PENALTY_FOR_BREAKING_TIME_WINDOW;
     }
 
     @Override
@@ -27,10 +49,10 @@ public class TimeWindowLowHighObjectiveFunction implements IObjectiveFunctionInt
         else {
             long timeWindowBreak = objectiveInfo.getVisitEnd() - objectiveInfo.getTask().getEndTime();
             if (timeWindowBreak <= highPenaltyCutOff)
-                return Math.max(0, timeWindowBreak);
+                return (long) (lowPenaltyMultiplier * Math.max(0, timeWindowBreak));
             else {
                 long highPenaltyTimeWindowBreak = timeWindowBreak - highPenaltyCutOff;
-                return highPenaltyCutOff + highPenaltyTimeWindowBreak * highPenaltyMultiplier;
+                return (long) (lowPenaltyMultiplier * highPenaltyCutOff + highPenaltyTimeWindowBreak * highPenaltyMultiplier);
             }
         }
     }
